31fa529dc2487b46adabca27ac2a5f314abe7602
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-12-13 15:45:17 +0100

    inizializzazione esercizio con vite + creato nuovo componente ma non funzionante

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,14 @@
+<template lang="">
+    <div>
+        <h1>
+            La mia prima app con Vite!
+        </h1>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+</style>
